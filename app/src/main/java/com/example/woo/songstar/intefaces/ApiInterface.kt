4cb0a13b1d7580894a4f80c7a14efe8b629ae802
Sergio Castrillon <scastrillon@watchourown.com>
2021-03-04 08:40:29 -0500

    removed unsued import

--- app/src/main/java/com/example/woo/songstar/intefaces/ApiInterface.kt
+++ app/src/main/java/com/example/woo/songstar/intefaces/ApiInterface.kt
@@ -3,7 +3,6 @@ package com.example.woo.songstar.intefaces
 import com.example.woo.songstar.models.ItunesAPIResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiInterface {
